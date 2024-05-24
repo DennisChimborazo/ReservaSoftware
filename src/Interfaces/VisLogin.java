f5dd3fb9fe1e09ac2d4b7d3e80fc0dd227e63f14
Michelle <mtunja8609@uta.edu.ec>
2024-05-24 16:29:40 -0500

    Se realizo la interfaz principal, coloco el banner e imagenes en los botones

--- src/Interfaces/VisLogin.java
+++ src/Interfaces/VisLogin.java
@@ -76,7 +76,7 @@ public class VisLogin extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 680, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
--- src/Interfaces/VisPrincipal.java
+++ src/Interfaces/VisPrincipal.java
@@ -8,18 +8,36 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 
 /**
  *
  * @author Dalex
  */
 public class VisPrincipal extends javax.swing.JFrame {
+        int Xmov, Ymov;
+
 
     /**
      * Creates new form VisPrincipal
      */
     public VisPrincipal() {
         initComponents();
+        ImageIcon icono = new ImageIcon(getClass().getResource( "/Imagenes/banner1.jpg"));
+        jLabel1.setIcon(icono);
+        ImageIcon iconoa = new ImageIcon(getClass().getResource( "/Imagenes/aulas.png"));
+        jbtnAulas.setIcon(iconoa);
+        ImageIcon iconol = new ImageIcon(getClass().getResource( "/Imagenes/laboratorio.png"));
+        jbtnLaboratorio1.setIcon(iconol);
+        ImageIcon iconot = new ImageIcon(getClass().getResource( "/Imagenes/taller.png"));
+        jbtnTaller.setIcon(iconot);
+        ImageIcon iconoli = new ImageIcon(getClass().getResource( "/Imagenes/lista_reserva.png"));
+        jbtnReservar.setIcon(iconoli);
+        ImageIcon iconoau = new ImageIcon(getClass().getResource( "/Imagenes/auditorios.png"));
+        jbtnAuditorio.setIcon(iconoau);
+        
     }
 
     /**
@@ -36,7 +54,7 @@ public class VisPrincipal extends javax.swing.JFrame {
         jbtnAulas = new javax.swing.JButton();
         jbtnTaller = new javax.swing.JButton();
         jbtnLaboratorio1 = new javax.swing.JButton();
-        jbtnTaller1 = new javax.swing.JButton();
+        jbtnReservar = new javax.swing.JButton();
         jbtnAuditorio = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         ImageIcon icon = new ImageIcon(getClass().getResource("/Imagenes/fondo_escritorio.png"));
@@ -46,71 +64,79 @@ public class VisPrincipal extends javax.swing.JFrame {
                 g.drawImage(imagen,0,0,getWidth(), getHeight(),this);
             }
         };
+        jPanel3 = new javax.swing.JPanel();
+        jPnl_salida = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
+        jPnl_min = new javax.swing.JPanel();
+        jLbl_min = new javax.swing.JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/banner1.jpg"))); // NOI18N
-        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1212, -1));
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 30, 1212, -1));
 
         jPanel1.setBackground(new java.awt.Color(110, 7, 7));
         jPanel1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        jbtnAulas.setBackground(new java.awt.Color(110, 7, 7));
         jbtnAulas.setFont(new java.awt.Font("Microsoft Uighur", 1, 36)); // NOI18N
-        jbtnAulas.setForeground(new java.awt.Color(255, 255, 255));
+        jbtnAulas.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/aulas.png"))); // NOI18N
         jbtnAulas.setText("Aulas");
-        jbtnAulas.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jbtnAulas.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jbtnAulas.setBackground(Color.WHITE);
 
-        jbtnTaller.setBackground(new java.awt.Color(110, 7, 7));
         jbtnTaller.setFont(new java.awt.Font("Microsoft Uighur", 1, 36)); // NOI18N
-        jbtnTaller.setForeground(new java.awt.Color(255, 255, 255));
+        jbtnTaller.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/taller.png"))); // NOI18N
         jbtnTaller.setText("Talleres");
-        jbtnTaller.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jbtnTaller.setMaximumSize(new java.awt.Dimension(68, 46));
-        jbtnTaller.setMinimumSize(new java.awt.Dimension(68, 46));
-        jbtnTaller.setPreferredSize(new java.awt.Dimension(68, 46));
+        jbtnTaller.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jbtnTaller.setMaximumSize(new java.awt.Dimension(110, 44));
+        jbtnTaller.setMinimumSize(new java.awt.Dimension(110, 44));
+        jbtnTaller.setBackground(Color.WHITE);
+        jbtnTaller.setPreferredSize(new java.awt.Dimension(110, 44));
 
-        jbtnLaboratorio1.setBackground(new java.awt.Color(110, 7, 7));
         jbtnLaboratorio1.setFont(new java.awt.Font("Microsoft Uighur", 1, 36)); // NOI18N
-        jbtnLaboratorio1.setForeground(new java.awt.Color(255, 255, 255));
+        jbtnLaboratorio1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/laboratorio.png"))); // NOI18N
         jbtnLaboratorio1.setText("Laboratorios");
-        jbtnLaboratorio1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jbtnLaboratorio1.setMaximumSize(new java.awt.Dimension(68, 46));
-        jbtnLaboratorio1.setMinimumSize(new java.awt.Dimension(68, 46));
-        jbtnLaboratorio1.setPreferredSize(new java.awt.Dimension(68, 46));
+        jbtnLaboratorio1.setToolTipText("");
+        jbtnLaboratorio1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jbtnLaboratorio1.setMaximumSize(new java.awt.Dimension(110, 44));
+        jbtnLaboratorio1.setMinimumSize(new java.awt.Dimension(110, 44));
+        jbtnLaboratorio1.setPreferredSize(new java.awt.Dimension(110, 44));
+        jbtnLaboratorio1.setBackground(Color.WHITE);
 
-        jbtnTaller1.setBackground(new java.awt.Color(110, 7, 7));
-        jbtnTaller1.setFont(new java.awt.Font("Microsoft Uighur", 1, 36)); // NOI18N
-        jbtnTaller1.setForeground(new java.awt.Color(255, 255, 255));
-        jbtnTaller1.setText("Reservar");
-        jbtnTaller1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jbtnTaller1.setMaximumSize(new java.awt.Dimension(68, 46));
-        jbtnTaller1.setMinimumSize(new java.awt.Dimension(68, 46));
-        jbtnTaller1.setPreferredSize(new java.awt.Dimension(68, 46));
+        jbtnReservar.setFont(new java.awt.Font("Microsoft Uighur", 1, 36)); // NOI18N
+        jbtnReservar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/lista_reserva.png"))); // NOI18N
+        jbtnReservar.setText("Lista Reserva");
+        jbtnReservar.setToolTipText("");
+        jbtnReservar.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jbtnReservar.setMaximumSize(new java.awt.Dimension(110, 44));
+        jbtnReservar.setBackground(Color.WHITE);
+        jbtnReservar.setMinimumSize(new java.awt.Dimension(110, 44));
+        jbtnReservar.setPreferredSize(new java.awt.Dimension(110, 44));
 
-        jbtnAuditorio.setBackground(new java.awt.Color(110, 7, 7));
         jbtnAuditorio.setFont(new java.awt.Font("Microsoft Uighur", 1, 36)); // NOI18N
-        jbtnAuditorio.setForeground(new java.awt.Color(255, 255, 255));
-        jbtnAuditorio.setText("Auditorio");
-        jbtnAuditorio.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        jbtnAuditorio.setMaximumSize(new java.awt.Dimension(68, 46));
-        jbtnAuditorio.setMinimumSize(new java.awt.Dimension(68, 46));
-        jbtnAuditorio.setPreferredSize(new java.awt.Dimension(68, 46));
+        jbtnAuditorio.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/auditorios.png"))); // NOI18N
+        jbtnAuditorio.setText("Auditorios");
+        jbtnAuditorio.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jbtnAuditorio.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
+        jbtnAuditorio.setMaximumSize(new java.awt.Dimension(110, 44));
+        jbtnAuditorio.setMinimumSize(new java.awt.Dimension(110, 44));
+        jbtnAuditorio.setBackground(Color.WHITE);
+        jbtnAuditorio.setPreferredSize(new java.awt.Dimension(110, 44));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jbtnAuditorio, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbtnTaller1, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbtnAulas, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbtnLaboratorio1, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbtnTaller, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(34, Short.MAX_VALUE))
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(jbtnAuditorio, javax.swing.GroupLayout.DEFAULT_SIZE, 227, Short.MAX_VALUE)
+                    .addComponent(jbtnReservar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jbtnTaller, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jbtnLaboratorio1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jbtnAulas, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(23, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -122,13 +148,13 @@ public class VisPrincipal extends javax.swing.JFrame {
                 .addGap(18, 18, 18)
                 .addComponent(jbtnTaller, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jbtnTaller1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jbtnReservar, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jbtnAuditorio, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(119, Short.MAX_VALUE))
+                .addComponent(jbtnAuditorio, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(70, Short.MAX_VALUE))
         );
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 150, 240, 660));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 180, 260, 610));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
 
@@ -138,11 +164,11 @@ public class VisPrincipal extends javax.swing.JFrame {
         jdskEscritorio.setLayout(jdskEscritorioLayout);
         jdskEscritorioLayout.setHorizontalGroup(
             jdskEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 958, Short.MAX_VALUE)
+            .addGap(0, 938, Short.MAX_VALUE)
         );
         jdskEscritorioLayout.setVerticalGroup(
             jdskEscritorioLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 618, Short.MAX_VALUE)
+            .addGap(0, 598, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -162,10 +188,109 @@ public class VisPrincipal extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
-        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 150, 970, 630));
+        getContentPane().add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 180, 950, 610));
+
+        jPanel3.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel3.setPreferredSize(new java.awt.Dimension(530, 420));
+        jPanel3.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                jPanel3MouseDragged(evt);
+            }
+        });
+        jPanel3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                jPanel3MousePressed(evt);
+            }
+        });
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPnl_salida.setBackground(new java.awt.Color(255, 255, 255));
+        jPnl_salida.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPnl_salidaMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jPnl_salidaMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jPnl_salidaMouseExited(evt);
+            }
+        });
+        jPnl_salida.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel2.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
+        jLabel2.setText("   X");
+        jPnl_salida.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 30, 30));
+
+        jPanel3.add(jPnl_salida, new org.netbeans.lib.awtextra.AbsoluteConstraints(1160, 0, 50, 30));
+
+        jPnl_min.setBackground(new java.awt.Color(255, 255, 255));
+        jPnl_min.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPnl_minMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                jPnl_minMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                jPnl_minMouseExited(evt);
+            }
+        });
+        jPnl_min.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLbl_min.setBackground(new java.awt.Color(0, 0, 0));
+        jLbl_min.setFont(new java.awt.Font("Rockwell", 1, 14)); // NOI18N
+        jLbl_min.setText("   -");
+        jPnl_min.add(jLbl_min, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 0, 30, 30));
+
+        jPanel3.add(jPnl_min, new org.netbeans.lib.awtextra.AbsoluteConstraints(1110, 0, 50, 30));
+
+        getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1210, 30));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jPnl_salidaMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPnl_salidaMouseClicked
+        int mensaje = JOptionPane.showConfirmDialog(null, "¿Está seguro que desea salir?", "Confirmación de salida", JOptionPane.YES_NO_OPTION);
+        if (mensaje==JOptionPane.YES_OPTION) {
+            JOptionPane.showMessageDialog(null, "Salió del sistema");
+            System.exit(0);
+        }
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPnl_salidaMouseClicked
+
+    private void jPnl_salidaMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPnl_salidaMouseEntered
+        jPnl_salida.setBackground(Color.red);
+    }//GEN-LAST:event_jPnl_salidaMouseEntered
+
+    private void jPnl_salidaMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPnl_salidaMouseExited
+        jPnl_salida.setBackground(Color.WHITE);
+    }//GEN-LAST:event_jPnl_salidaMouseExited
+
+    private void jPnl_minMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPnl_minMouseClicked
+        setExtendedState(JFrame.ICONIFIED);
+
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPnl_minMouseClicked
+
+    private void jPnl_minMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPnl_minMouseEntered
+        jPnl_min.setBackground(Color.LIGHT_GRAY);
+    }//GEN-LAST:event_jPnl_minMouseEntered
+
+    private void jPnl_minMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPnl_minMouseExited
+        jPnl_min.setBackground(Color.white);
+    }//GEN-LAST:event_jPnl_minMouseExited
+
+    private void jPanel3MouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MouseDragged
+        int x = evt.getXOnScreen();
+        int y = evt.getYOnScreen();
+        this.setLocation(x - Xmov, y - Ymov);
+    }//GEN-LAST:event_jPanel3MouseDragged
+
+    private void jPanel3MousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel3MousePressed
+        Xmov = evt.getX();
+        Ymov = evt.getY();
+    }//GEN-LAST:event_jPanel3MousePressed
 
     /**
      * @param args the command line arguments
@@ -204,13 +329,18 @@ public class VisPrincipal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLbl_min;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JButton jbtnAuditorio;
-    private javax.swing.JButton jbtnAulas;
-    private javax.swing.JButton jbtnLaboratorio1;
-    private javax.swing.JButton jbtnTaller;
-    private javax.swing.JButton jbtnTaller1;
-    private javax.swing.JDesktopPane jdskEscritorio;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPnl_min;
+    public javax.swing.JPanel jPnl_salida;
+    public javax.swing.JButton jbtnAuditorio;
+    public javax.swing.JButton jbtnAulas;
+    public javax.swing.JButton jbtnLaboratorio1;
+    public javax.swing.JButton jbtnReservar;
+    public javax.swing.JButton jbtnTaller;
+    public javax.swing.JDesktopPane jdskEscritorio;
     // End of variables declaration//GEN-END:variables
 }

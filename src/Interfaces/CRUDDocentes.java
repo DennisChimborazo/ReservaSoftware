6c2ba071c5350a52ad6789fe22e9c3f6c387bbde
Eduardo <edupunina12@gmail.com>
2024-06-10 00:15:40 -0500

    Se agrego la fucionalidad de ditar y eliminar docente

--- src/Interfaces/CRUDDocentes.java
+++ src/Interfaces/CRUDDocentes.java
@@ -11,13 +11,17 @@ import Validadores.Validadores;
 import java.awt.HeadlessException;
 import javax.swing.table.DefaultTableModel;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  *
  * @author edupu
  */
-public class CRUDDocentes extends javax.swing.JInternalFrame {
+public class CRUDDocentes extends javax.swing.JFrame {
 
     Docente doc;
 
@@ -25,15 +29,39 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
         initComponents();
         textosBlancos();
         cargarTabla("");
-        //this.setLocationRelativeTo(null);
+        bloquearTextos();
+        this.setLocationRelativeTo(null);
+        selecionarTabla();
     }
 
-    public void textosBlancos() {
+    private void textosBlancos() {
         this.jtxtCedula.setText("");
         this.jtxtNombre.setText("");
         this.jtxtTelefono.setText("");
         this.jtxtDireccion.setText("");
         this.jtxtBuscar.setText("");
+    }
+
+    private void bloquearTextos() {
+        this.jbtnGuardar.setEnabled(false);
+        this.jbtnEditar.setEnabled(false);
+        this.jbtnCancelar.setEnabled(false);
+        this.jtxtCedula.setEnabled(false);
+        this.jtxtNombre.setEnabled(false);
+        this.jtxtTelefono.setEnabled(false);
+        this.jtxtDireccion.setEnabled(false);
+        this.jbtnNuevo.setEnabled(true);
+    }
+
+    private void btnNuevo() {
+        this.jtxtCedula.setEnabled(true);
+        this.jtxtNombre.setEnabled(true);
+        this.jtxtTelefono.setEnabled(true);
+        this.jtxtDireccion.setEnabled(true);
+        this.jbtnGuardar.setEnabled(true);
+        this.jbtnCancelar.setEnabled(true);
+        this.jbtnNuevo.setEnabled(false);
+        this.jbtnEliminar.setEnabled(true);
     }
 
     private void cargarTabla(String dato) {
@@ -106,8 +134,9 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
                                         if (rs > 0) {
                                             JOptionPane.showMessageDialog(null, "Se ingreso correctamene");
                                             cargarTabla("");
+                                            textosBlancos();
+                                            bloquearTextos();
                                             ps.close();
-
                                         }
                                     } catch (HeadlessException | SQLException e) {
                                         System.out.println(e.toString());
@@ -133,6 +162,76 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
 
     }
 
+    private void editarDocente() {
+
+        if (!this.jtxtCedula.getText().isEmpty() && !this.jtxtNombre.getText().isEmpty() && !this.jtxtTelefono.getText().isEmpty() && !this.jtxtDireccion.getText().isEmpty()) {
+            String sql = "UPDATE personas SET  telf_per='" + this.jtxtTelefono.getText() + "', dir_per='" + this.jtxtDireccion.getText() + "' WHERE id_per='" + this.jtxtCedula.getText() + "'";
+            try {
+                PreparedStatement ps = Conexion.getConnection().prepareStatement(sql);
+                int res = ps.executeUpdate();
+                if (res > 0) {
+                    JOptionPane.showMessageDialog(null, "Se actualizo correctamene");
+                    cargarTabla("");
+                    textosBlancos();
+                    bloquearTextos();
+                    ps.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(CRUDDocentes.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+
+        }
+    }
+
+    private void eliminarDocente() {
+        String sql;
+        try {
+            sql = "Delete from usuarios where id_per ='" + this.jtxtCedula.getText() + "'";
+            PreparedStatement ps = Conexion.getConnection().prepareStatement(sql);
+            int res = ps.executeUpdate();
+            if (res > 0) {
+                sql = "Delete from personas where id_per ='" + this.jtxtCedula.getText() + "'";
+                ps = Conexion.getConnection().prepareStatement(sql);
+                res = ps.executeUpdate();
+                if (res > 0) {
+                    JOptionPane.showMessageDialog(null, "Se Elimino correctamene");
+                    cargarTabla("");
+                    textosBlancos();
+                    bloquearTextos();
+                    ps.close();
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    private void selecionarTabla() {
+        this.jTable1.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                int row = jTable1.getSelectedRow();
+                if (row != -1) {
+                    jtxtCedula.setText(jTable1.getValueAt(row, 0).toString());
+                    jtxtNombre.setText(jTable1.getValueAt(row, 1).toString());
+                    jtxtTelefono.setText(jTable1.getValueAt(row, 2).toString());
+                    jtxtDireccion.setText(jTable1.getValueAt(row, 3).toString());
+                    jbtnGuardar.setEnabled(false);
+                    jbtnEditar.setEnabled(true);
+                    jbtnCancelar.setEnabled(true);
+                    jtxtCedula.setEnabled(false);
+                    jtxtNombre.setEnabled(false);
+                    jtxtTelefono.setEnabled(true);
+                    jtxtDireccion.setEnabled(true);
+                    jbtnNuevo.setEnabled(false);
+                    jbtnEliminar.setEnabled(true);
+                }
+            }
+        });
+    }
+
     public static void main(String[] args) {
         CRUDDocentes doc = new CRUDDocentes();
         doc.setVisible(true);
@@ -142,6 +241,7 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPopupMenu1 = new javax.swing.JPopupMenu();
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
@@ -157,8 +257,9 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
         jtxtBuscar = new javax.swing.JTextField();
         jbtnNuevo = new javax.swing.JButton();
         jbtnGuardar = new javax.swing.JButton();
-        jbntEditar = new javax.swing.JButton();
+        jbtnEditar = new javax.swing.JButton();
         jbtnCancelar = new javax.swing.JButton();
+        jbtnEliminar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
 
@@ -188,6 +289,11 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
         jtxtBuscar.setText("jTextField5");
 
         jbtnNuevo.setText("Nuevo");
+        jbtnNuevo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnNuevoMouseClicked(evt);
+            }
+        });
 
         jbtnGuardar.setText("Guardar");
         jbtnGuardar.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -196,9 +302,26 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
             }
         });
 
-        jbntEditar.setText("Editar");
+        jbtnEditar.setText("Editar");
+        jbtnEditar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnEditarMouseClicked(evt);
+            }
+        });
 
         jbtnCancelar.setText("Cancelar");
+        jbtnCancelar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnCancelarMouseClicked(evt);
+            }
+        });
+
+        jbtnEliminar.setText("Eliminar");
+        jbtnEliminar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jbtnEliminarMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -222,10 +345,11 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
                     .addComponent(jtxtBuscar))
                 .addGap(135, 135, 135)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jbtnNuevo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jbtnNuevo, javax.swing.GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE)
                     .addComponent(jbtnGuardar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jbntEditar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jbtnCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE))
+                    .addComponent(jbtnEditar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jbtnCancelar, javax.swing.GroupLayout.DEFAULT_SIZE, 111, Short.MAX_VALUE)
+                    .addComponent(jbtnEliminar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -246,17 +370,23 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(jtxtTelefono, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbntEditar))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel7)
-                    .addComponent(jtxtDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jbtnCancelar))
-                .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel8)
-                    .addComponent(jtxtBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(0, 11, Short.MAX_VALUE))
+                    .addComponent(jbtnEditar))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel7)
+                            .addComponent(jtxtDireccion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel8)
+                            .addComponent(jtxtBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(0, 18, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jbtnEliminar)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jbtnCancelar)
+                        .addGap(27, 27, 27))))
         );
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
@@ -297,6 +427,24 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
         agregarDocente();
     }//GEN-LAST:event_jbtnGuardarMouseClicked
 
+    private void jbtnNuevoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnNuevoMouseClicked
+        btnNuevo();
+        textosBlancos();
+    }//GEN-LAST:event_jbtnNuevoMouseClicked
+
+    private void jbtnCancelarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnCancelarMouseClicked
+        bloquearTextos();
+        textosBlancos();
+    }//GEN-LAST:event_jbtnCancelarMouseClicked
+
+    private void jbtnEditarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnEditarMouseClicked
+        editarDocente();
+    }//GEN-LAST:event_jbtnEditarMouseClicked
+
+    private void jbtnEliminarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jbtnEliminarMouseClicked
+        eliminarDocente();
+    }//GEN-LAST:event_jbtnEliminarMouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
@@ -307,10 +455,12 @@ public class CRUDDocentes extends javax.swing.JInternalFrame {
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPopupMenu jPopupMenu1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
-    private javax.swing.JButton jbntEditar;
     private javax.swing.JButton jbtnCancelar;
+    private javax.swing.JButton jbtnEditar;
+    private javax.swing.JButton jbtnEliminar;
     private javax.swing.JButton jbtnGuardar;
     private javax.swing.JButton jbtnNuevo;
     private javax.swing.JTextField jtxtBuscar;
